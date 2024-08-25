7c310e75ff0b3843f18f99a2c0e6d917d2bd8354
34342343 <vzeldak07@gmail.com>
2024-08-25 13:48:03 +0300

    feat: move Firebase config to environment variables for security

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,13 @@ import {
 } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAf7L8JKGp7R7bxmQlq7WXT-KFPH8dd81A',
-  authDomain: 'test-vue-registration.firebaseapp.com',
-  projectId: 'test-vue-registration',
-  storageBucket: 'test-vue-registration.appspot.com',
-  messagingSenderId: '447436341358',
-  appId: '1:447436341358:web:319ddf4ecea6c8bb5f667f',
-  measurementId: 'G-SW6PWE1WT0'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
